487e08f4e4bec492f1bcd5e3b504a3d8526d5f17
Ronald Chevarria <renato120397@gmail.com>
2022-07-07 08:52:44 -0500

    implementacion de pruebas unitarias

--- TestUnitario/UnitTest1.cs
+++ TestUnitario/UnitTest1.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using System.Net;
 using Microsoft.Extensions.Configuration;
 using MediatR;
-using Aplicacion.Categorias;
+using Aplicacion;
 using FluentValidation;
 using Persistencia;
 using System.Collections.Generic;
@@ -13,57 +13,107 @@ using Moq;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
 using Newtonsoft.Json;
-using APIPagos.Controllers;
+using System;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace TestUnitario
 {
     [TestClass]
     public class UnitTest1
     {
-        public static IConfiguration InitConfiguration()
+        /*public static IConfiguration InitConfiguration()
         {
             var config = new ConfigurationBuilder()
                .AddJsonFile("appsettings.Development.json")
                 .AddEnvironmentVariables()
                 .Build();
             return config;
-        }
-
-        public static string RaizURL()
-        {
-            var URL = "http://localhost:47113/api/";
-            return URL;
-        }
-
-        //prueba unitaria para validar acción de consulta
-        /*[TestMethod]
-        public async Task AplicacionConsulta_Test()
-        {
-            var moq = new Mock<IMediator>();
-            //Act
-            var controller = new CategoriasController();
-
-            //Arrange
-            var rpta = await controller.Get();
-
-            //something like:
-            Assert.IsNotNull(rpta);
         }*/
 
         //prueba unitaria para validacion fluent
-        /*[TestMethod]
-        public void AplicacionConsultaValidation_Test()
+        [TestMethod]
+        public void AplicacionConsultaIdCategoriaValidation_Test()
         {
             //Arrange
-            var consulta = new ConsultaId.CategoriaUnica();
+            var consulta = new Aplicacion.Categorias.ConsultaId.CategoriaUnica();
             consulta.Id_categoria = 1000;
 
             //Act
-            var servicio = new ConsultaId.EjecutaValidacion();
+            var servicio = new Aplicacion.Categorias.ConsultaId.EjecutaValidacion();
             var servicioRpta = servicio.Validate(consulta);
 
             //Assert
-            Assert.IsFalse(servicioRpta.IsValid);
-        }*/
+            Assert.IsTrue(servicioRpta.IsValid);
+        }
+
+        //prueba unitaria para validacion fluent
+        [TestMethod]
+        public void AplicacionConsultaIdDocumentoValidation_Test()
+        {
+            //Arrange
+            var consulta = new Aplicacion.Documentos.ConsultaId.DocumentoUnico();
+            consulta.id_documento = 500;
+
+            //Act
+            var servicio = new Aplicacion.Documentos.ConsultaId.EjecutaValidacion();
+            var servicioRpta = servicio.Validate(consulta);
+
+            //Assert
+            Assert.IsTrue(servicioRpta.IsValid);
+        }
+
+        //prueba unitaria para validacion fluent
+        [TestMethod]
+        public void AplicacionConsultaIdPagoValidation_Test()
+        {
+            //Arrange
+            var consulta = new Aplicacion.Pagos.ConsultaId.PagoUnico();
+            consulta.Id_pago = 500;
+
+            //Act
+            var servicio = new Aplicacion.Pagos.ConsultaId.EjecutaValidacion();
+            var servicioRpta = servicio.Validate(consulta);
+
+            //Assert
+            Assert.IsTrue(servicioRpta.IsValid);
+        }
+
+        //prueba unitaria para validacion fluent
+        [TestMethod]
+        public void AplicacionLoginUsuarioValidation_Test()
+        {
+            //Arrange
+            var consulta = new Aplicacion.Seguridad.Login.Ejecuta();
+            consulta.Email = "RENATO@GMAIL.COM";
+            consulta.Password = "$$$$$CONTRASEÑA$$$$";
+
+            //Act
+            var servicio = new Aplicacion.Seguridad.Login.EjecutaValidacion();
+            var servicioRpta = servicio.Validate(consulta);
+
+            //Assert
+            Assert.IsTrue(servicioRpta.IsValid);
+        }
+
+        //prueba unitaria para validacion fluent
+        [TestMethod]
+        public void AplicacionRegistrarUsuarioValidation_Test()
+        {
+            //Arrange
+            var consulta = new Aplicacion.Seguridad.Registrar.Ejecuta();
+            consulta.Email = "RENATO@GMAIL.COM";
+            consulta.Password = "$$$$$CONTRASEÑA$$$$";
+            consulta.Nombre = "RENATO";
+            consulta.Username = "RENATO.CHEVA";
+            consulta.Apellidos = "CHEVARRIA CAMARGO";
+
+            //Act
+            var servicio = new Aplicacion.Seguridad.Registrar.EjecutaValidacion();
+            var servicioRpta = servicio.Validate(consulta);
+
+            //Assert
+            Assert.IsTrue(servicioRpta.IsValid);
+        }
+
     }
 }
